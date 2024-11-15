98e735e9c0d4998d1aea3c1666121210d88bf781
inhyeok <alzpqm@naver.com>
2024-11-16 00:34:44 +0900

    init: init project

---
+++ src/main/java/com/data_bobu/dinner_sheet_server/DinnerSheetServerApplication.java
@@ -0,0 +1,13 @@
+package com.data_bobu.dinner_sheet_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerSheetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnerSheetServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data_bobu/dinner_sheet_server/DinnerSheetServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.data_bobu.dinner_sheet_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnerSheetServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
